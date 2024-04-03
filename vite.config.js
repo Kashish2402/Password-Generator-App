cecf422dd7c162b06b31045d0842d9e1ecff3172
Kashish2402 <68956193+Kashish2402@users.noreply.github.com>
2024-04-03 16:46:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   build: {
+    rollupOptions: {
+      input: {
+        main: '/src/main.jsx'
+      }
+    }
+  },
   plugins: [react()],
 })
+
+
+
